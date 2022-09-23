0214296020f78f2dd55b8205d71f2bcefd2d1447
jennifermendozasalinas10 <107737779+jennifermendozasalinas10@users.noreply.github.com>
2022-09-23 16:41:57 -0300

    Add files via upload

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Item = ({info}) => {
+  return (
+    <>
+       <Link to={`/detalle/${info.id}`} className='film'>
+         <img src={info.image} alt='Gorros de lana'/>
+         <p>info.title</p>
+       </Link>
+    </>
+  );
+}
+
+export default Item;
