be76977870d979709f08b8e642dbcca4858115ef
jennifermendozasalinas10 <107737779+jennifermendozasalinas10@users.noreply.github.com>
2022-09-26 14:04:50 -0300

    Add files via upload

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+
+export const CartWidget = () => {
+
+  
+  return (
+    <>
+       <i className="bi bi-cart3"></i>
+    </>
+  );
+}
+
+
+export default CartWidget;
